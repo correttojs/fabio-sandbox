78809fbefb0771c3717657f9660690416620e056
Fabio Benedetti <fabio.benedetti@vercel.com>
2025-06-17 16:36:25 +0200

    timeout

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,6 +5,7 @@ export async function GET() {
 
     const cookieStore = await cookies()
     cookieStore.set('myCookie', new Date().toISOString())
+    await new Promise(resolve => setTimeout(resolve, 7000)) // Simulate some delay
     return new Response(
         JSON.stringify({
             message: 'Hello, World!',
